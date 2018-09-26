1451dab49876d9b2183d3c6118e3e858850df98d
kasambuli <cynthiakasambuli@gmail.com>
2018-09-26 14:31:16 +0300

    added id and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuid from 'uuid';
 import './App.css';
 import Projects from './Components/projects';
 import AddProject from './Components/addprojects';
@@ -16,26 +17,46 @@ class App extends Component {
     componentWillMount() {
         this.setState({
             projects: [{
+                    id: uuid.v4(),
                     title: "Business Website",
                     category: "web design"
                 },
                 {
-                    title: "Business Website",
-                    category: "web design"
+                    id: uuid.v4(),
+                    title: "School Website",
+                    category: "mobile design"
                 },
                 {
-                    title: "Business Website",
-                    category: "web design"
+                    id: uuid.v4(),
+                    title: "Hospital Website",
+                    category: "android design"
                 }
             ]
         });
-
     }
+    handleAddProject(project) {
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({ projects: projects });
+    };
+    handledeleteProject(id) {
+        let projects = this.state.projects;
+        let index = projects.findIndex(x => x.id === id);
+        projects.splice(index, 1)
+        this.setState({ projects: projects });
+    }
+
     render() {
         return ( < div className = "App" >
             <
-            AddProject / >
-            My App < Projects projects = { this.state.projects }
+            AddProject addProject = { this.handleAddProject.bind(this) }
+            / >
+            My App < Projects projects = {
+                this.state.projects
+            }
+            onDelete = {
+                this.handledeleteProject.bind(this)
+            }
             / > < /
             div > );
     }
